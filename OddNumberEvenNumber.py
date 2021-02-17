53455e6970ac2441609147b36dd389e300434df5
shourov-sorkar <18103142@iubat.edu>
2021-02-18 04:16:37 +0600

    :1234: check if the input number is odd or even

---
+++ OddNumberEvenNumber.py
@@ -0,0 +1,6 @@
+num = int(input("Enter the value: ")) #take input from user 
+
+if (num % 2) == 0:  #Condition 
+    print("This is Even Number") # print the result 
+else:
+    print("This is Odd Number")
