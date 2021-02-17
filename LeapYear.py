40b810c37f33f8f828f455ff606bd43c1d0c3a95
shourov-sorkar <18103142@iubat.edu>
2021-02-18 04:30:42 +0600

    :zap: Add formatter

--- LeapYear.py
+++ LeapYear.py
@@ -1,12 +1,13 @@
 year = int(input("Enter the Year: ")) # take input from user 
 
-if(year % 4) == 0:
-    if(year % 100) == 0:
-        if(year % 400) == 0:
-            print("This is Leap Year")
+if(year % 4) == 0: #condition year is divisible by 4 or not 
+    if(year % 100) == 0: #condition year is divisible by 100 or not 
+        if(year % 400) == 0:  #condition year is divisible by 400 or not 
+            print("{0} is Leap Year".format(year)) #print when condition is true 
+        #print when condition is false 
         else:
-            print("This is Not Leap Year")
-    else:
-        print("This is not Leap Year")
+            print("{0} is Not Leap Year".format(year)) 
+    else: 
+        print("{0} is not Leap Year".format(year))
 else:
-    print("This is not Leap Year")
+    print("{0} is not Leap Year".format(year)) 
