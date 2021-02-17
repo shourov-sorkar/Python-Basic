77c6adb17bfa7fd4fecbacf3b2b480d635d1adec
shourov-sorkar <18103142@iubat.edu>
2021-02-18 04:09:38 +0600

    :repeat: Swapping the values

---
+++ SwappingTheValues.py
@@ -0,0 +1,9 @@
+a = input("Enter the value of X: ") # take input from user
+b = input("Enter the value of Y: ") # take input from user 
+
+temp = a #take the temporary variable 
+a = b
+b = temp 
+
+print('The value of x after swapping: {}'.format(a)) #print the results 
+print('The value of y after swapping: {}'.format(b))
