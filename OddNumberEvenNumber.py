059f335964be1f14de4bdacf32442deffcfb23c6
shourov-sorkar <18103142@iubat.edu>
2021-02-18 04:26:05 +0600

    :1234: Add  Formatters

--- OddNumberEvenNumber.py
+++ OddNumberEvenNumber.py
@@ -1,6 +1,6 @@
 num = int(input("Enter the value: ")) #take input from user 
 
 if (num % 2) == 0:  #Condition 
-    print("This is Even Number") # print the result 
+    print("{0} Even Number".format(num)) # print the result 
 else:
-    print("This is Odd Number")
+    print("{0} is Odd Number".format(num))
